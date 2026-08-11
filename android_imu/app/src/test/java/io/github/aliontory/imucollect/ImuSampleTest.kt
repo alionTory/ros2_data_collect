563f441b30dd09b6cfd04fc7bc3b55910235cb2c
오건호 <58841595+alionTory@users.noreply.github.com>
2026-08-11 18:18:03 +0900

    ImuSample 테스트 코드 작성.

---
+++ android_imu/app/src/test/java/io/github/aliontory/imucollect/ImuSampleTest.kt
@@ -0,0 +1,27 @@
+package io.github.aliontory.imucollect
+
+import android.hardware.SensorManager
+import org.junit.Assert
+import org.junit.Test
+
+class ImuSampleTest {
+    @Test
+    fun encodeTest() {
+        val messageType = ImuSample.TYPE_IMU_ACCEL
+        val nowNs = System.nanoTime()
+        val accuracy = SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM.toByte()
+        val x = 0f
+        val y = 0f
+        val z = 0f
+        val imuSample = ImuSample(messageType, nowNs, accuracy, x, y, z)
+        val byteArray = imuSample.encode(0)
+
+        val failMessage = "앞 4 바이트(payload_len)가 00 00 00 0D (십진수 13) 여야 함."
+        Assert.assertEquals(failMessage, 0x00.toByte(), byteArray[0])
+        Assert.assertEquals(failMessage, 0x00.toByte(), byteArray[1])
+        Assert.assertEquals(failMessage, 0x00.toByte(), byteArray[2])
+        Assert.assertEquals(failMessage, 0x0D.toByte(), byteArray[3])
+
+        Assert.assertEquals("메시지 타입이 보존되어야 함", messageType, byteArray[4])
+    }
+}
